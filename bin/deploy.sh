b9753a5250b9c584b4605d08cb7d11c3fd5908cf
kurudrive <kurudrive@gmail.com>
2017-11-14 21:27:49 +0900

    echo '<br />_|＼○_ﾋｬｯ ε=＼＿○ﾉ ﾎｰｳ!!'.'<br />'."\n";

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,7 +4,7 @@
 #     echo "Not deploying pull requests."
 #     exit
 # fi
-
+echo "_|＼○_ﾋｬｯ ε=＼＿○ﾉ ﾎｰｳ!!"
 if [[ "dist_test" != "$TRAVIS_BRANCH" ]]; then
     echo "Not on the 'master' branch."
     exit
@@ -12,6 +12,7 @@ fi
 
 set -e
 
+## -b オプションはチェックアウト
 git clone -b dist --quiet "https://github.com/${TRAVIS_REPO_SLUG}.git" dist
 npm run dist
 cd dist
