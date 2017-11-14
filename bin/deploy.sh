0ba686b89fba7d021fdbb1a6d8afbba9d1728673
kurudrive <kurudrive@gmail.com>
2017-11-14 22:55:29 +0900

    デバッグ削除

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,7 +4,6 @@
 #     echo "Not deploying pull requests."
 #     exit
 # fi
-echo "_|＼○_ﾋｬｯ ε=＼＿○ﾉ ﾎｰｳ!!"
 if [[ "dist_test" != "$TRAVIS_BRANCH" ]]; then
     echo "Not on the 'master' branch."
     exit
@@ -19,6 +18,4 @@ cd dist
 ## すべての変更を含むワークツリーの内容をインデックスに追加.
 git add -A
 git commit -m "Update from travis $TRAVIS_COMMIT"
-echo "_|＼○_ﾋｬｯ ε=＼＿○ﾉ ﾎｰｳ!! 2"
 git push --quiet "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" dist 2> /dev/null
-echo "_|＼○_ﾋｬｯ ε=＼＿○ﾉ ﾎｰｳ!! 3"
