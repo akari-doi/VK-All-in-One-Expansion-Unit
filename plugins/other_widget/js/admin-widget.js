8af28a37ecdfc6da7928623983dc61db18d5af8d
kurudrive <kurudrive@gmail.com>
2018-01-05 19:49:05 +0900

    カラーピッカーテスト

--- plugins/other_widget/js/admin-widget.js
+++ plugins/other_widget/js/admin-widget.js
@@ -46,3 +46,107 @@ if ( vk_widget_image_del == undefined ){
 		jQuery(e).parent().children("._form").children('._url').attr("value","").change();
 	};
 }
+
+
+
+
+
+
+( function( $, api ) {
+    api( 'blogname', function( setting ) {
+        setting.bind( function( value ) {
+            $( '.site-title a' ).text( value );
+        } );
+    } );
+} ( jQuery, wp.customize ) );
+
+
+jQuery(document).ready(function(i){
+
+
+		    // Short-circuit selective refresh events if not in customizer preview or pre-4.5.
+	    // if ( 'undefined' === typeof wp || ! wp.customize || ! wp.customize.selectiveRefresh ) {
+	    //     return;
+	    // }
+
+	console.log('そもそもここは読んでる？');
+
+			// // テキストが変更された時
+			// wp.customize( 'lightning_theme_options[header_top_contact_txt]', function( contact_txt_value ) {
+			// 	contact_txt_value.bind( function( contact_txt_new_val ) {
+			// 		// グローバルのテキストを書き換えておく
+			// 		// contact_txt = contact_txt_value.get();
+			// 		contact_txt = contact_txt_new_val;
+			// 		// console.log( contact_txt_new_val + ' : ' + contact_url );
+			// 		header_top_contact_btn( contact_txt, contact_url );
+			// 	} );
+			// } );
+
+	    // Re-load Twitter widgets when a partial is rendered.
+	    wp.customize.selectiveRefresh.bind( 'partial-content-rendered', function( placement ) {
+				console.log('placement:'+placement);
+	        if ( placement.container ) {
+
+						console.log('_|＼○_ﾋｬｯ ε=＼＿○ﾉ ﾎｰｳ!!');
+	            // twttr.widgets.load( placement.container[0] );
+	        }
+	    } );
+
+	    // Refresh a moved partial containing a Twitter timeline iframe, since it has to be re-built.
+	    wp.customize.selectiveRefresh.bind( 'partial-content-moved', function( placement ) {
+	        if ( placement.container && placement.container.find( 'iframe.twitter-timeline:not([src]):first' ).length ) {
+	            placement.partial.refresh();
+	        }
+	    } );
+
+
+/////////////////
+
+
+
+
+
+
+	console.log('うえ-----------い');
+	jQuery('.color_picker_wrap').click(function(){
+		console.log('うい-----------い');
+	});
+
+	// $(document).off('click','p');
+	// $('.color_picker_wrap').on('click','input',function(){
+	//      console.log('押した');
+	// });
+	// $('.color_picker_wrap').off('click','.wp-picker-clear');
+	// jQuery('.wp-picker-clear').click();
+	//
+		// $(document).on('click','.color_picker_wrap',function(e){
+		//       console.log('外を押した');
+		// 			jQuery(e).find('input.wp-color-picker').attr("value","").change();
+		// });
+		$('.color_picker_wrap').click(function(){
+			console.log('外２を押した');
+			jQuery(this).find('input.wp-color-picker').attr("value","").change();
+		});
+		// $(document).on('click','p',function(){
+		// 			console.log('pタグ押した');
+		// });
+		$(document).on('click','.wp-picker-input-wrap',function(){
+					console.log('wp-picker-input-wrap タグ押した');
+		});
+	jQuery('.wp-picker-clear').ready(function(i){
+		console.log('クリア読みました');
+		$('.wp-picker-clear').on('click',function(){
+					console.log('クリア押した');
+		});
+		$('.wp-picker-clear').click(function(){
+					console.log('クリア押した');
+		});
+	});
+		// $(document).on('click','.wp-picker-input-wrap',function(){
+		// 			console.log('wp-picker-input-wrap タグ押した');
+		// });
+    //
+		// $(document).on('click','.wp-picker-clear',function(){
+		// 			console.log('クリア押した');
+		// });
+});
